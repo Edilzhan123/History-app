9a09d0b6b4091b0d69e2a7644f53c8f30bf7e0f3
Edilzhan <edilzhan123456@gmail.com>
2026-04-18 21:12:38 +0500

    fix routing correctly

--- src/pages/TopicDetail.jsx
+++ src/pages/TopicDetail.jsx
@@ -13,31 +13,37 @@ function TopicDetail() {
     <div>
       <h1>{topic.title}</h1>
       <p>{topic.content}</p>
+
       <div className="baseBook">
-        <a
-          className="linkToBase"
-          href={topic.pdfBase}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src="https://store-images.s-microsoft.com/image/apps.45573.13510798886655750.7655cc79-0d25-41ea-b75f-b64c7120201f.cf626021-9fc0-4803-8c89-151943ba155b"
-            alt=""
-          />
-          <p>БАЗА</p>
-        </a>
-        <a
-          className="linkToBook"
-          href={topic.pdfBook}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src="https://store-images.s-microsoft.com/image/apps.45573.13510798886655750.7655cc79-0d25-41ea-b75f-b64c7120201f.cf626021-9fc0-4803-8c89-151943ba155b"
-            alt=""
-          />
-          <p>КІТАП</p>
-        </a>
+        {topic.pdfBase && (
+          <a
+            className="linkToBase"
+            href={topic.pdfBase}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="https://store-images.s-microsoft.com/image/apps.45573.13510798886655750.7655cc79-0d25-41ea-b75f-b64c7120201f.cf626021-9fc0-4803-8c89-151943ba155b"
+              alt=""
+            />
+            <p>БАЗА</p>
+          </a>
+        )}
+
+        {topic.pdfBook && (
+          <a
+            className="linkToBook"
+            href={topic.pdfBook}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="https://store-images.s-microsoft.com/image/apps.45573.13510798886655750.7655cc79-0d25-41ea-b75f-b64c7120201f.cf626021-9fc0-4803-8c89-151943ba155b"
+              alt=""
+            />
+            <p>КІТАП</p>
+          </a>
+        )}
       </div>
     </div>
   );
